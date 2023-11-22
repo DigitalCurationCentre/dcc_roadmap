b4bb0205079a9c51e6d42f0eecf8bf7d309ec2f1
gjacob24 <gjacob@ed.ac.uk>
2023-11-22 12:10:22 +0000

    Fixed issue with answer format dropdown, add options and add conditions

--- app/javascript/src/orgAdmin/conditions/updateConditions.js
+++ app/javascript/src/orgAdmin/conditions/updateConditions.js
@@ -24,12 +24,15 @@ export default function updateConditions(id) {
   const allowWebhook = (selectObject, webhook = false) => { // webhook false => new condition
     const condition = $(selectObject).closest('.condition-partial');
     console.log("condition-partial", condition);
+    console.log("selectobject val:", $(selectObject).val());
     if (webhook === false) {
       if ($(selectObject).val() === 'add_webhook') { // condition type is webhook
         condition.find('.pseudo-webhook-btn').trigger('click');
+        console.log("added webhook");
       } else { // condition type is remove
         condition.find('.remove-dropdown').show();
         condition.find('.webhook-replacement').hide();
+        console.log("couldn't add webhook");
       }
     } else { // loading already saved conditions
       // populate webhook inputs
