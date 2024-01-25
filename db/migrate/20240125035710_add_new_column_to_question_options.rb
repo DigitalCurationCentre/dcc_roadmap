f11b7f05f82ddb78c2959f104c0d5340699bd369
Marta Nicholson <marta.nicholson@ed.ac.uk>
2024-01-25 04:38:15 +0000

    DCC-149_question_id_answer_indentifiers

--- db/migrate/20240125035710_add_new_column_to_question_options.rb
+++ db/migrate/20240125035710_add_new_column_to_question_options.rb
@@ -1,5 +1,5 @@
 class AddNewColumnToQuestionOptions < ActiveRecord::Migration[6.1]
   def change
-    add_column :question_options, :answer_identifer, :string
+    add_column :question_options, :answer_identifier, :string
   end
 end
