c03e5adb26cc49851a924ae1714fc6d8641a92a2
dstuckey-uoe <dstuckey@ed.ac.uk>
2024-01-28 23:57:12 +0000

    Corrected referencing of foreign key.

--- db/migrate/20240128233752_add_api_server_reference_to_templates.rb
+++ db/migrate/20240128233752_add_api_server_reference_to_templates.rb
@@ -1,5 +1,5 @@
 class AddApiServerReferenceToTemplates < ActiveRecord::Migration[6.1]
   def change
-    add_column :templates, :api_servers, :foreign_key
+    add_reference :templates, :api_servers, foreign_key: true
   end
 end
