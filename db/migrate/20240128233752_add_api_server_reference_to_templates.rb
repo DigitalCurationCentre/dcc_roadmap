de507bf889e047b4b5bd5f50b59b10fd2d8c8216
dstuckey-uoe <dstuckey@ed.ac.uk>
2024-01-31 09:15:45 +0000

    Corrected name of foreign key to the singular.

--- db/migrate/20240128233752_add_api_server_reference_to_templates.rb
+++ db/migrate/20240128233752_add_api_server_reference_to_templates.rb
@@ -1,5 +1,5 @@
 class AddApiServerReferenceToTemplates < ActiveRecord::Migration[6.1]
   def change
-    add_reference :templates, :api_servers, foreign_key: true
+    add_reference :templates, :api_server, foreign_key: true
   end
 end
