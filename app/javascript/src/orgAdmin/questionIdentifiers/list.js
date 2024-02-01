6f9e99de76ad5ba7d69a65e06ab895790d9d1d69
Marta Nicholson <marta.nicholson@ed.ac.uk>
2024-02-01 11:40:10 +0000

    small commit with the modal dialog

--- app/javascript/src/orgAdmin/questionIdentifiers/list.js
+++ app/javascript/src/orgAdmin/questionIdentifiers/list.js
@@ -1,11 +1,34 @@
 import { isObject, isString } from '../../utils/isType';
 import { renderNotice, renderAlert } from '../../utils/notificationHelper';
 import { scrollTo } from '../../utils/scrollTo';
-
-$(() => {
-    $(document).ready(function() {
-        $('#questionIdentifiersListModal').on('hidden.bs.modal', function () {
-          alert('I am here');
+$(document).ready(function() {
+   
+    // $('#questionIdentifiersListModal').remove(); // Remove any existing modal with the same ID
+    $('body').append(response.html); 
+    alert ("1 alert");
+    $('body').on('click','#link_to_question_identifiers_list', (e) => {
+        $('#questionIdentifiersListModal').modal('show');
+        
+        $('#questionIdentifiersListModal').on('ajax:success', function(event, data, status, xhr) {
+        if (data.success) {
+            $('#questionIdentifiersListBody').html(data.html); // Update modal content with rendered partial
+            alert(data.message);
+        } else {
+            // Handle errors
+        }
         });
-      });
+    });    
 });
+
+
+// $(() => {
+//     alert("1 here");
+//     $('body').on('click','#link_to_question_identifiers_list', (e) => {
+//        alert("2 here");
+//        $(document).ready(function() {
+//             $('#questionIdentifiersListModal').on('show.bs.modal', function () {
+//                 alert("3 here");
+//              });        
+//         });
+//     });
+// });
